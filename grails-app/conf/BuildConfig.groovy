4a48d536685e924b17ae9aaa3fa79bcad0bc77c9
Ingo Wiarda <ingo_wiarda@dewarim.de>
2014-09-20 14:13:20 +0200

    Upgrade plugins.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -4,13 +4,13 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.7
 grails.project.source.level = 1.7
+grails.project.war.file = "target/${appName}.war"
+grails.project.dependency.resolver = "maven"
 
 // uncomment (and adjust settings) to fork the JVM to isolate classpaths
 //grails.project.fork = [
 //   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
 //]
-
-grails.project.dependency.resolver = "maven"
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
@@ -23,42 +23,24 @@ grails.project.dependency.resolution = {
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
     repositories {
-        inherits true // Whether to inherit repository definitions from plugins
-
-        grailsPlugins()
-        grailsHome()
         grailsCentral()
-
         mavenLocal()
         mavenCentral()
-
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+//        mavenRepo 'http://repo.spring.io/milestone'
     }
-
+    
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-
-        // runtime 'mysql:mysql-connector-java:5.1.20'
         runtime 'postgresql:postgresql:9.1-901.jdbc4'
     }
 
     plugins {
-        build ':tomcat:7.0.42'
-        runtime ":hibernate:3.6.10.12"
-        runtime ":jquery:1.8.3"
-        runtime ":resources:1.2"
+        build ':tomcat:7.0.54'
+        runtime ":hibernate:3.6.10.17"
+        build':release:3.0.0', ':rest-client-builder:1.0.3', {
+            export = false
+        }
+        compile ":asset-pipeline:1.9.7"
 
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.4"
-
-        runtime ":database-migration:1.2.1"
-        compile ':cache:1.0.1'
-        compile ':goblin:0.3.3'
+        compile ':goblin:0.4.0.3'
     }
 }
